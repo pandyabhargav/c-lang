66fd40f70ab179445f0ecbce6f1f465a12028743
Deepdomadiya4646 <deepdomadiya4646@gmail.com>
2023-07-25 13:49:49 +0530

    wap-if-els

---
+++ if-else/if-else.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+	//1
+int a, b;
+
+printf("enter a = ");
+scanf("%d", &a);
+
+printf("enter b = ");
+scanf("%d", &b);
+
+   if(a > b){
+   printf("b is minimum..\n");	
+   }else{
+   	printf("a is minimum\n");
+   }
+   //2
+}
